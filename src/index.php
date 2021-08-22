29d624795443cc3769223fce41203817b4a37b94
datamaskinmann <svens@uia.no>
2021-08-22 17:16:57 +0200

    Omgjort til struktur som ønskes av lærer

--- src/index.php
+++ src/index.php
@@ -12,10 +12,10 @@
 	<div class="menu">
 	<h3>Modul 1</h3>
 		<ul class="menuUl">
-			<li><a href="/phpInfo.php"/>Oppgave 2 php.ini</a></li>
-			<li><a href="/alder.php"/>Oppgave 3 Alder</a></li>
-			<li><a href="/calc.php"/>Oppgave 4 Kalkulator</a></li>
-			<li><a href="/seconds.php">Oppgave 5 Sekunder</a></li>
+			<li><a href="/oppgave2.php"/>Oppgave 2 php.ini</a></li>
+			<li><a href="/oppgave3.php"/>Oppgave 3 Alder</a></li>
+			<li><a href="/oppgave4.php"/>Oppgave 4 Kalkulator</a></li>
+			<li><a href="/oppgave5.php">Oppgave 5 Sekunder</a></li>
 		</ul>
 	</div>
  </body>
